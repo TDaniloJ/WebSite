432a708686eb17d16bcd9822766d0d32034f4b49
Danilo Dias <131262817+TDaniloJ@users.noreply.github.com>
2024-02-27 11:18:09 -0300

    script login
    script do login ainda em desevolvimento

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+function showCadastro() {
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('registro').style.display = 'block';
+}
 
+function showLogin() {
+    document.getElementById('login').style.display = 'block';
+    document.getElementById('registro').style.display = 'none';
+}
